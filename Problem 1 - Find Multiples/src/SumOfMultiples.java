2cae8fa1627a080e34b999a335d5171d315048d8
JonGeorge <23463258+JonGeorge@users.noreply.github.com>
2018-12-31 10:51:26 -0500

    Modified method to return int

--- Problem 1 - Find Multiples/src/SumOfMultiples.java
+++ Problem 1 - Find Multiples/src/SumOfMultiples.java
@@ -1,16 +1,19 @@
 /*
  * Problem 1
- * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
- * The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.
+ *
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5,
+ * we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ *
+ * Find the sum of all the multiples of 3 or 5 below 1000.
  */
 public class SumOfMultiples {
 
     public static void main(String[] args) {
         SumOfMultiples sumOfMultiples = new SumOfMultiples();
-        sumOfMultiples.findSumOfMultiples();
+        System.out.println("Sum: " + sumOfMultiples.findSumOfMultiples());
     }
 
-    private void findSumOfMultiples() {
+    private int findSumOfMultiples() {
         int count = 3;
         int sum = 0;
 
@@ -23,6 +26,6 @@ public class SumOfMultiples {
             }
             count++;
         }
-        System.out.println("Sum: " + sum);
+        return sum;
     }
 }
